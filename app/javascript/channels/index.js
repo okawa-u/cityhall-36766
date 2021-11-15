b5c3e072e0499815cc6c3f17c1897aa07eac96b9
Okawa <pandanyanya@gmail.com>
2021-11-15 12:42:36 +0900

    Google Maps APIのJS CSS追記

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,12 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
